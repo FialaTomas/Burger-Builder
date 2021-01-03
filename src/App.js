56327acfe27399228e62e1550fce593b33e69f84
Tomas Fiala <tomasfiala@yahoo.com>
2021-01-03 21:42:33 +0100

    BurgerIngredients

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import BurgerIngredient from "./components/Burger/BurgerIngredient/BurgerIngredient";
+// import Layout from "./components/Layout/Layout";
+import Layout from "./components/Layout/Layout";
+
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
