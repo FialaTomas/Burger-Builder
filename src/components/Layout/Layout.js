9bd9a15a2701d61d603104a1bbda10cb36074720
Tomas Fiala <tomasfiala@yahoo.com>
2021-01-05 14:34:38 +0100

    Nav SideDrawer
    -added side drawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <div>SideDrawer, Backdrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSidedrawer: true,
+  };
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSidedrawer: false });
+  };
 
-export default layout;
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSidedrawer: !prevState.showSidedrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSidedrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <div>SideDrawer, Backdrop</div>
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
